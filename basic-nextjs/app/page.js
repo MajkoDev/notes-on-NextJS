b6d3c314ac4506cb4d57b31f928cf89dca0adc08
Marian <hellomariandev@gmail.com>
2023-09-14 20:30:33 +0200

    defining routes

--- basic-nextjs/app/page.js
+++ basic-nextjs/app/page.js
@@ -1,3 +1,7 @@
+import { Metadata } from "next";
+
+export const metadata = { title: "basic NextJS" };
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return <h1>Hello, Next.js!</h1>;
 }
